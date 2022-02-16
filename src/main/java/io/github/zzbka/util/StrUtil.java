e30a6385753a87296e26b6d19e92153acd6cc74e
拽拽 <1050564479@qq.com>
2022-02-16 19:46:42 +0800

    init

---
+++ src/main/java/io/github/zzbka/util/StrUtil.java
@@ -0,0 +1,12 @@
+package io.github.zzbka.util;
+
+/**
+ * @author cs
+ * @des
+ * @date 2022/2/16
+ */
+public class StrUtil {
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+}
